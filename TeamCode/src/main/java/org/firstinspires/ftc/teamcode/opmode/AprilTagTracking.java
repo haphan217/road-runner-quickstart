cc5f50cfb258a59c1e04ee91bb59a441abd6b936
haphan217 <haphankhanh217@gmail.com>
2025-10-12 09:42:56 +0700

    track apriltag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AprilTagTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AprilTagTracking.java
@@ -5,31 +5,48 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 import java.util.List;
-
+@Autonomous
 public class AprilTagTracking extends LinearOpMode {
     private Limelight3A limelight;
-    public DcMotor motor;
+    public DcMotor Front_Right_Drive;
+    public DcMotor Front_Left_Drive;
+    public DcMotor Back_Right_Drive;
+    public DcMotor Back_Left_Drive;
+    double kP = 0.05;
+    double kD = 0.001;
+    ElapsedTime timer = new ElapsedTime();
+    private double lastTx = 0;
 
-    double limelightAimProportional(double tx)
+
+    double pidController(double tx)
     {
-        double kP = .015;
-        double targetingAngularVelocity = tx * kP;
-        targetingAngularVelocity *= -1.0;
-        return targetingAngularVelocity;
+        double derivative = (tx - lastTx) / (timer.seconds());
+        lastTx = tx;
+        timer.reset();
+        return tx * kP + derivative * kD;
     }
 
     @Override
     public void runOpMode() throws InterruptedException
     {
-        motor = hardwareMap.get(DcMotor.class, "motor");
+
+        Front_Right_Drive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        Front_Left_Drive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        Back_Right_Drive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        Back_Left_Drive = hardwareMap.get(DcMotor.class, "left_back_drive");
+
+        Back_Right_Drive.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
         FtcDashboard.getInstance().startCameraStream(limelight, 0);
@@ -41,21 +58,46 @@ public class AprilTagTracking extends LinearOpMode {
 
         while (opModeIsActive()) {
             LLResult result = limelight.getLatestResult();
-            List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
-            for (LLResultTypes.FiducialResult fiducial : fiducials) {
-                int id = fiducial.getFiducialId(); // The ID number of the fiducial
-                double tx = fiducial.getTargetXDegrees(); // Where it is (left-right)
-                double ty = fiducial.getTargetYDegrees(); // Where it is (up-down)
-                double rotation = limelightAimProportional(tx);
+            double tx= result.getTx();
+            double rotation = pidController(tx);
 
-                telemetry.addData("tx", tx);
-                telemetry.addData("rotation", rotation);
-                telemetry.addData("ty", ty);
-                telemetry.update();
+            telemetry.addData("tx", tx);
+            telemetry.addData("rotation", rotation);
 
-                motor.setPower(rotation);
 
+            if(Math.abs(tx) > 3) {
+                Front_Left_Drive.setPower(rotation);
+                Back_Left_Drive.setPower(rotation);
+                Front_Right_Drive.setPower(-rotation);
+                Back_Right_Drive.setPower(-rotation);
+            } else {
+                Front_Left_Drive.setPower(0);
+                Back_Left_Drive.setPower(0);
+                Front_Right_Drive.setPower(0);
+                Back_Right_Drive.setPower(0);
             }
+
+            telemetry.update();
+
+//            List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
+//            for (LLResultTypes.FiducialResult fiducial : fiducials) {
+//                int id = fiducial.getFiducialId(); // The ID number of the fiducial
+//                double tx = fiducial.getTargetXDegrees(); // Where it is (left-right)
+//                double ty = fiducial.getTargetYDegrees(); // Where it is (up-down)
+//                double rotation = pidController(tx);
+//
+//                telemetry.addData("tx", tx);
+//                telemetry.addData("rotation", rotation);
+//                telemetry.addData("id", id);
+//                telemetry.update();
+
+
+//                Front_Left_Drive.setPower(rotation*0.3);
+//                Back_Left_Drive.setPower(rotation*0.3);
+//                Front_Right_Drive.setPower(rotation*-0.3);
+//                Back_Right_Drive.setPower(rotation*-0.3);
+
+//            }
 
 
         }
